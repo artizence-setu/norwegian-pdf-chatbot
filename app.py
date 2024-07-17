5fde19779459fa123b243cd1a6b99e096b0eb17c
Ashirwad Kankaria <kankariashirwad@gmail.com>
2024-07-17 12:46:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,92 +1,81 @@
-from flask import Flask, request, jsonify
-import re
-import threading
-import uuid
-from scrapper import scrape, scrape_status
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+import httpx
+import os
 
-app = Flask(__name__)
+app = FastAPI()
 
-conversations = {}
+origins = ["*"]
 
-def is_valid_email(email):
-    return re.match(r"[^@]+@[^@]+\.[^@]+", email)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-def is_valid_phone(phone):
-    return re.match(r"^\+?\d{10,15}$", phone)
+class WebhookRequestData(BaseModel):
+    object: str = ""
+    entry: List = []
 
-def is_valid_url(url):
-    return re.match(r"https?://www\.finn\.no/realestate/.*", url)
 
-@app.route('/chatbot', methods=['POST'])
-def chatbot():
-    data = request.get_json()
-    conversation_id = data.get('conversation_id')
-    message = data.get('message')
+@app.get("/")
+def read_root():
+    return Response(content="FB Bot Messanger api")
 
-    if not conversation_id:
-        conversation_id = str(uuid.uuid4())
-        conversations[conversation_id] = {"step": "hello", "user_info": {}}
-    
-    conversation = conversations[conversation_id]
-    user_info = conversation["user_info"]
 
-    print(f"Current step: {conversation['step']}")  # Debugging statement
-    print(f"Message: {message}")  # Debugging statement
-    
-    if conversation["step"] == "hello":
-        conversation["step"] = "ask_name"
-        return jsonify(conversation_id=conversation_id, response="Hello! Please provide your name:")
-
-    if conversation["step"] == "ask_name":
-        user_info['name'] = message
-        conversation["step"] = "ask_phone"
-        return jsonify(conversation_id=conversation_id, response="Please provide your phone number:")
-
-    if conversation["step"] == "ask_phone":
-        if is_valid_phone(message):
-            user_info['phone'] = message
-            conversation["step"] = "ask_house_link"
-            return jsonify(conversation_id=conversation_id, response="Please provide the house link:")
+@app.get("/webhook")
+def verifyHook(req: Request, res: Response):
+    mode = req._query_params.get('hub.mode')
+    token = req._query_params.get('hub.verify_token')
+    challenge = req._query_params.get('hub.challenge')
+    if(mode and token):
+        if (mode == "subscribe" and token == os.environ['AUTH_TOKEN']):
+            print('Verified request.')
+            return Response(content=challenge, status_code=200)
         else:
-            return jsonify(conversation_id=conversation_id, response="Invalid phone number format. Please provide a valid phone number:")
+            return Response(content="Cannot authenticat", status_code=401)
+    return Response(content="Parameters not found.", status_code=400)
 
-    if conversation["step"] == "ask_house_link":
-        if is_valid_url(message):
-            user_info['house_link'] = message
-            conversation["step"] = "ask_email"
-            return jsonify(conversation_id=conversation_id, response="Please provide your email:")
-        else:
-            return jsonify(conversation_id=conversation_id, response="I only work with finn.no links. Please provide a valid house link:")
 
-    if conversation["step"] == "ask_email":
-        if is_valid_email(message):
-            user_info['email'] = message
-            response_message = f"Thank you, {user_info['name']}! Here is the information you provided:\n" \
-                               f"Email: {user_info['email']}\n" \
-                               f"Phone: {user_info['phone']}\n" \
-                               f"House Link: {user_info['house_link']}\n" \
-                               f"Please wait while I scan the info....."
-            response = jsonify(conversation_id=conversation_id, response=response_message)
-            threading.Thread(target=scrape, args=(user_info['house_link'], conversation_id)).start()
-            return response
-        else:
-            return jsonify(conversation_id=conversation_id, response="Invalid email format. Please provide a valid email:")
 
-    return jsonify(conversation_id=conversation_id, response="Hello! Please provide your name:")
+async def sendMessage(
+    page_access_token: str,
+    recipient_id: str,
+    message_text: str,
+    message_type: str = "UPDATE"
+    ):
+    r = httpx.post(
+        "https://graph.facebook.com/v19.0/me/messages",
+        params={"access_token": page_access_token},
+        headers={"Content-Type": "application/json"},
+        json={
+            "recipient": {"id": recipient_id},
+            "message": {"text": message_text},
+            "messaging_type": message_type,
+        },
+    )
+    r.raise_for_status()
 
-@app.route('/chatbot/status', methods=['GET'])
-def check_status():
-    conversation_id = request.args.get('conversation_id')
-    status = scrape_status.get(conversation_id, "pending")
-    if status == "completed":
-        return jsonify(conversation_id=conversation_id, response="Scraping completed. You can now ask your questions.")
-    elif status == "error":
-        return jsonify(conversation_id=conversation_id, response="Scraping encountered an error. Please try again.")
-    else:
-        return jsonify(conversation_id=conversation_id, response="Scraping is still in progress. Please wait.")
 
-# def main():
-#     app.run(debug=True)
-
-# if __name__ == '__main__':
-#     main()
+@app.post('/webhook')
+async def handleWebhook(data : WebhookRequestData):
+    print("Event received.")
+    if data.object == "page":
+        for entry in data.entry:
+            messaging_events = [ event for event in entry.get("messaging", []) if event.get("message") ]
+            for event in messaging_events:
+                message = event.get("message")
+                sender_id = event["sender"]["id"]
+                if(sender_id == os.environ['SELF_ID']):
+                    print("Received event for self message. Returning status 200.")
+                    return Response(content="success", status_code=200)
+                print(sender_id, message, event)
+                await sendMessage(page_access_token=os.environ["ACCESS_TOKEN"],
+                                   recipient_id=sender_id,
+                                   message_text="Hello world",
+                                   message_type="RESPONSE")
+    return Response(content="success", status_code=200)
